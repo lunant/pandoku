51aa1257edfa9f58ed1c585f461f7fbbfac36796
Hong, MinHee <minhee@dahlia.kr>
2009-10-08 23:39:58 +0900

    Added LaTeX format.

---
+++ lib/pandoku/formats/latex.rb
@@ -0,0 +1,27 @@
+require File.dirname(File.dirname(__FILE__)) + '/format'
+
+module Pandoku::Formats
+  # LaTeX format. http://docutils.sourceforge.net/rst.html
+  #
+  # == Available Options
+  #
+  # <tt>:parse_raw</tt>:: Parse untranslatable LaTeX environments as raw LaTeX,
+  #                       instead of ignoring them.
+  # <tt>:number_sections</tt>:: Number section headings in LaTeX output.
+  #                             (Default is not to number them.)
+  # <tt>:no_wrap</tt>:: Disable text wrapping in output.
+  #                     (Default is to wrap text.)
+  # <tt>:toc</tt>:: Include an instruction to create table of contents.
+  class LaTeX < Pandoku::Format
+    include Pandoku::InputFormat, Pandoku::OutputFormat
+
+    def self.name
+      :latex
+    end
+
+    def self.default_options
+      { :parse_raw => false, :number_sections => false, :no_wrap => false,
+        :toc => false }
+    end
+  end
+end
